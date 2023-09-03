95feaa2b04e3cf9b5db19a777b9e6186b9073ee7
Ertan K <tosss.beee@gmail.com>
2023-09-03 11:28:11 +0200

    fixing the constructor parameters

--- pytorch/question-answering/run_qa.py
+++ pytorch/question-answering/run_qa.py
@@ -259,12 +259,7 @@ def main():
     tokenizer.pad_token = tokenizer.eos_token
     model = LlamaForCausalLM.from_pretrained(
         model_args.model_name_or_path,
-        from_tf=bool(".ckpt" in model_args.model_name_or_path),
         config=config,
-        cache_dir=model_args.cache_dir,
-        revision=model_args.model_revision,
-        token=model_args.token,
-        trust_remote_code=model_args.trust_remote_code,
     )
 
     # Tokenizer check: this script requires a fast tokenizer.
